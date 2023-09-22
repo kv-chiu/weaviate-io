3827098181645ebff348ff4291f0d9ae4dc5e294
Dave Cuthbert <david@weaviate.io>
2023-09-22 13:30:22 -0400

    export

--- _build_scripts/publish-draft-to-netlify.sh
+++ _build_scripts/publish-draft-to-netlify.sh
@@ -6,11 +6,8 @@ mv build/robots.txt build/robots.txt.live
 echo -e "User-agent: *
 Disallow: /" >> build/robots.txt
 
-# Get the current branch
-export CURRENT_BRANCH=$(git branch --show-current)
-
 # sudo netlify deploy --dir=build --site=tangerine-buttercream-20c32f >> netlify.out
-./node_modules/.bin/netlify deploy --dir=build --alias ${CURRENT_BRANCH}--site=tangerine-buttercream-20c32f > netlify.out
+./node_modules/.bin/netlify deploy --dir=build --alias "some-string-test" --site=tangerine-buttercream-20c32f > netlify.out
 
 # Bring back the original robots.txt file
 rm build/robots.txt
